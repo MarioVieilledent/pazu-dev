ed4ff994f1e1cc9e8a0620c6cd0553c4570b4549
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-12-14 15:56:20 +0100

    aled

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Flex, Text } from "@chakra-ui/react";
-import { Test } from "./components/Test";
+import { Box, Flex } from "@chakra-ui/react";
+import { Resizable } from "./utils/Resizable";
+const border = "1px solid black";
 
 function App() {
   return (
     <Flex direction="column" w="100%" h="100%">
-      <Text>Pazu.dev</Text>
-      <Test />
+      <Flex w="100%" h="50px" borderBottom={border}>
+        header
+      </Flex>
+      <Resizable
+        orientation="horizontal"
+        left={<Box>Nav</Box>}
+        right={<Box>Content</Box>}
+      />
     </Flex>
   );
 }
